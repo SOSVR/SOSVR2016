207fa2e83f93ec721929916a742c7abfb3cb6e2b
taherahmadi <14taher@gmail.com>
2016-05-15 10:33:53 +0430

    bring_up compeleted

---
+++ bring_up/gz_env.sh
@@ -0,0 +1,7 @@
+#. /usr/share/gazebo/setup.sh
+# export GAZEBO_PLUGIN_PATH=`pwd`/lib:${GAZEBO_PLUGIN_PATH}
+# export GAZEBO_MODEL_PATH=`pwd`/models:${GAZEBO_MODEL_PATH}
+export GAZEBO_PLUGIN_PATH=`pwd`/lib
+export GAZEBO_MODEL_PATH=`pwd`/src/bring_up/models
+#source /opt/ros/indigo/setup.bash
+#source /opt/ros/hydro/setup.bash
